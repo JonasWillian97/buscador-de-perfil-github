bc614fd3cc11e65457e755d8163368555243c3b0
Jonas Willian <jwnarancity@gmail.com>
2023-01-25 14:15:34 -0300

    Projeto buscador de perfil com api do github concluído

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App () {
+
+  const [search, setSearch] = useState('');
+  const [info, setInfo] = useState(null);
+
+  const handleSearch = () => {
+    axios.get(`https://api.github.com/users/${search}`)
+    .then((res) => {
+      if(res.status === 200) {
+      return res
+      }
+    })
+  
+    .then((data) => {
+      setInfo(data);
+      setSearch('');
+    })
+
+  }
+
+
+  useEffect(() => {
+  handleSearch()
+  },[])
+  return(
+    <div className="main">
+      <h1>Buscador de Perfil do GitHub</h1>
+      <div className="search" >
+        <input className="input" type='text' placeholder='Buscar um username...' value={search} onChange={e => setSearch(e.target.value)} />
+        <button className="button" onClick={handleSearch}>Buscar</button>
+      </div>
+
+      {
+        info
+        ?
+        (
+      <div>
+        <div>
+          <div>
+          <img className="image" src={info.data.avatar_url} />
+            <h2 className="title-Name">{info.data.name}</h2>
+          </div>
+          <h3 className="bio">{info.data.bio}</h3>
+        </div>
+      </div>
+        )
+        :
+        null
+      }
+
     </div>
-  );
+  )
 }
 
 export default App;
